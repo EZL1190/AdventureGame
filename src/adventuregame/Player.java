5e2c1e656e9e4d0ceeecafdcc96f078e4d566d95
niichtsShaiaz <mathias@igel.it>
2017-03-20 11:36:16 +0100

    Methods added for moving

--- src/adventuregame/Player.java
+++ src/adventuregame/Player.java
@@ -4,6 +4,8 @@ public class Player
     private int gold;
     private int hp = 100;
     private String position = "0.0";
+    private int xPosition;
+    private int yPosition;
 
     public int getGold() {
         return gold;
@@ -26,12 +28,61 @@ public class Player
         String[] positions = position.split(".");
         int xPosition = Integer.parseInt(positions[0]);
         int yPosition = Integer.parseInt(positions[1]);
+        this.xPosition = xPosition;
+        this.yPosition = yPosition;
+    }
+    public void setPosition()
+    {
+        position = xPosition +"."+yPosition;
     }
     
+    public void m_North(Floor floor, Player player)
+    {
+        if(floor.isNorthExist())
+        {
+            yPosition++;
+            setPosition();
+        }
+        else 
+        {
+            System.out.println("Error invalid move.");
+        }
+    }
     public void m_east(Floor floor, Player player)
     {
-        
-        
+        if(floor.isEastExist())
+        {
+            xPosition++;
+            setPosition();
+        }
+        else 
+        {
+            System.out.println("Error invalid move.");
+        }
+    }
+    public void m_South(Floor floor, Player player)
+    {
+        if(floor.isSouthExist())
+        {
+            yPosition--;
+            setPosition();
+        }
+        else 
+        {
+            System.out.println("Error invalid move.");
+        }
+    }
+    public void m_West(Floor floor, Player player)
+    {
+        if(floor.isWestExist())
+        {
+            xPosition--;
+            setPosition();
+        }
+        else 
+        {
+            System.out.println("Error invalid move.");
+        }
     }
     
 }
