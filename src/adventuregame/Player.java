67496fb9c572fc484b44e68a58be35ba5c947733
niichtsShaiaz <mathias@igel.it>
2017-03-20 11:23:43 +0100

    meme

--- src/adventuregame/Player.java
+++ src/adventuregame/Player.java
@@ -2,7 +2,8 @@ package adventuregame;
 public class Player 
 {
     private int gold;
-    private int hp = 1;
+    private int hp = 100;
+    private String position = "0.0";
 
     public int getGold() {
         return gold;
@@ -19,4 +20,18 @@ public class Player
     public void setHp(int hp) {
         this.hp = hp;
     }
+    
+    public void getPosition(String position)
+    {
+        String[] positions = position.split(".");
+        int xPosition = Integer.parseInt(positions[0]);
+        int yPosition = Integer.parseInt(positions[1]);
+    }
+    
+    public void m_east(Floor floor, Player player)
+    {
+        
+        
+    }
+    
 }
