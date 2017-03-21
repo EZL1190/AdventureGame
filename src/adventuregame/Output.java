ddf324a5187776e89021261e34b01de57681c909
EZL1190 <e.z.l@live.com>
2017-03-21 14:05:42 +0100

    output

--- src/adventuregame/Output.java
+++ src/adventuregame/Output.java
@@ -5,15 +5,17 @@ import java.util.Scanner;
 public class Output 
 {
     Scanner userInput = new Scanner(System.in);
-    public void Help() 
-    {
-        System.out.println("You've asked for help, these are all the commands in the game:");
-        System.out.println("Type 'gold' to see how much gold you have.");
-        System.out.println("Type 'quit' to leave the game.");
-        System.out.println("You can go 4 directions, to go West type 'west', to go South type 'south' and so on. (West - Left, East - Right, North - Up, South - Down)");
-        System.out.println("As you might have figured out, sometimes you're not able to go a certain direction, try another and see where that might take you.");
+    Game game;
+    Player player;
+
+    public void setGame(Game game) {
+        this.game = game;
     }
-    
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+     
     public void floorDescription(Floor floor)
     {
         System.out.println(floor.getDescription());
@@ -32,14 +34,14 @@ public class Output
             System.out.println("-East");
     }
     
-    public void playerInput()
+    public void playerInput(Floor floor)
     {
         System.out.println("\nEnter command: ");
         String input = userInput.nextLine();
-        commands(input);
+        commands(input, floor);
     }
     
-    public void commands(String input)
+    public void commands(String input, Floor floor)
     {
         input = input.toLowerCase();
         switch(input)
@@ -57,19 +59,19 @@ public class Output
                 break;
                 
             case"north":
-                north();
+                north(floor);
                 break;
                 
             case"west":
-                west();
+                west(floor);
                 break;
                 
             case"south":
-                south();
+                south(floor);
                 break;
                 
             case"east":
-                east();
+                east(floor);
                 break;
                 
             default:
@@ -77,22 +79,37 @@ public class Output
         }
     }
     
-    public void quit() {
+    public void Help() 
+    {
+        System.out.println("You've asked for help, these are all the commands in the game:");
+        System.out.println("Type 'gold' to see how much gold you have.");
+        System.out.println("Type 'quit' to leave the game.");
+        System.out.println("You can go 4 directions, to go West type 'west', to go South type 'south' and so on. (West - Left, East - Right, North - Up, South - Down)");
+        System.out.println("As you might have figured out, sometimes you're not able to go a certain direction, try another and see where that might take you.");
+    }
+    
+    public void quit() 
+    {
         
     }
-    public void west() {
+    public void west(Floor floor) 
+    {
+        player.m_West(floor);
+    }
+    public void east(Floor floor) 
+    {
         
     }
-    public void east() {
+    public void south(Floor floor) 
+    {
         
     }
-    public void south() {
+    public void north(Floor floor) 
+    {
         
     }
-    public void north() {
-        
-    }
-    public void gold() {
+    public void gold() 
+    {
         
     }
 }
