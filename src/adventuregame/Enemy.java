bbdffcab02c081d8859ec55c32ebe2d5968206f4
niichtsShaiaz <mathias@igel.it>
2017-03-22 09:49:14 +0100

    enemy

--- src/adventuregame/Enemy.java
+++ src/adventuregame/Enemy.java
@@ -36,5 +36,21 @@ public class Enemy {
         this.name = type[rand.nextInt(type.length)];
         
     }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
     
 }
