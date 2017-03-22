aba1b381394fd4de797c2c740f425486f90e4ac1
EZL1190 <e.z.l@live.com>
2017-03-22 09:15:33 +0100

    change in inventory

--- src/adventuregame/Inventory.java
+++ src/adventuregame/Inventory.java
@@ -5,4 +5,26 @@ import java.util.HashMap;
 public class Inventory {
     HashMap<String, Equipment> equipment = new HashMap<String, Equipment>();
     HashMap<String, Item> item = new HashMap<String, Item>();
+    
+    public void setEquipment(String name, Equipment equ)
+    {
+        this.equipment.put(name, equ);
+    }
+    
+    public void setItem(String name, Item item)
+    {
+        this.item.put(name, item);
+    }
+    
+    public Equipment getEquipment(String name)
+    {
+        return this.equipment.get(name);
+    }
+    
+    public Item getItem(String name)
+    {
+        return this.item.get(name);
+    }
+    
+    
 }
