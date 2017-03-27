ebd64749cfb7555e8d30335f81cb5cd4b84c30fc
niichtsShaiaz <mathias@igel.it>
2017-03-27 09:54:23 +0200

    Highscore

---
+++ src/adventuregame/Highscore.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package adventuregame;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+
+/**
+ *
+ * @author Shxnna
+ */
+public class Highscore {
+    public void Highscore(Player player){
+     try {
+            FileWriter writer = new FileWriter("Scores", true);
+            writer.write("score: "+player.getGold());
+            writer.write("\r\n");   // write new line
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    
+
+    }
+
+}
