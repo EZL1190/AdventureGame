6f67b9e1e09e86dea18fb775b13ef220dc1a85e4
niichtsShaiaz <mathias@igel.it>
2017-04-07 08:53:47 +0200

    maybe it wurk

--- src/adventuregame/Highscore.java
+++ src/adventuregame/Highscore.java
@@ -6,26 +6,47 @@
 package adventuregame;
 
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
 
 /**
  *
  * @author Shxnna
  */
 public class Highscore {
-    public void Highscore(PlayerV2 player){
-     try {
-            FileWriter writer = new FileWriter("Scores", true);
-            writer.write("score: "+player.getGold());
-            writer.write("\r\n");   // write new line
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    
 
+    private static final File H_FILE = new File("highscore.txt");
+
+    public static void addScore(String name, int score) {
+        try (PrintWriter out = new PrintWriter(new FileWriter(H_FILE, true))) {
+            out.println(name + ": " + score);
+        } catch(IOException e) {
+            e.printStackTrace();
+
+        }
     }
 
+    public static ArrayList<String> getHighScores() throws FileNotFoundException {
+        try (Scanner in = new Scanner(new FileReader(H_FILE))) {
+            ArrayList<String> list = new ArrayList<>();
+            while (in.hasNext()) {
+                String line = in.nextLine();
+                list.add(line + "\n");
+
+            }
+        }
+    }
+
+    public void sortScores() {
+        Collections.sort(getHighScores());
+        Collections.reverse(getHighScores());
+
+    }
 }
