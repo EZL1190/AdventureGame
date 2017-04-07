ff28ba0af181ae199392541aa03b332cfc60239e
niichtsShaiaz <mathias@igel.it>
2017-04-07 09:31:27 +0200

    awd

--- src/adventuregame/Highscore.java
+++ src/adventuregame/Highscore.java
@@ -6,26 +6,64 @@
 package adventuregame;
 
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
 
 /**
  *
- * @author Shxnna/Gay
+ * @author Shxnna
  */
 public class Highscore {
-    public void Highscore(Player player){
-     try {
-            FileWriter writer = new FileWriter("Scores", true);
-            writer.write("score: "+player.getGold());
-            writer.write("\r\n");   // write new line
-            writer.close();
+
+    private static final File H_FILE = new File("highscore.txt");
+
+    public static void addScore(String name, int score) {
+        try (PrintWriter out = new PrintWriter(new FileWriter(H_FILE, true))) {
+            out.println(name + ": " + score);
         } catch (IOException e) {
             e.printStackTrace();
-        }
-    
 
+        }
     }
 
+    public static ArrayList<String> getHighScores() {
+        try (Scanner in = new Scanner(new FileReader(H_FILE))) {
+            ArrayList<String> list = new ArrayList<>();
+            while (in.hasNext()) {
+                String line = in.nextLine();
+                list.add(line + "\n");
+            }
+            return list;
+        } catch (IOException ex) {
+            if (true) {
+                ex.printStackTrace();
+            }
+            System.exit(1);
+            return null; // Unreachable
+        }
+    }
+
+    public void sortScores() throws FileNotFoundException {
+        Collections.sort(getHighScores());
+        Collections.reverse(getHighScores());
+
+    }
+    
+    @Override
+    public String toString(){
+        String s = "";
+        for (int i = 0; i<getHighScores().size(); i++){
+            s = "" + getHighScores().get(i) + "\n";
+            
+        }
+        return s;
+        
+    }
 }
